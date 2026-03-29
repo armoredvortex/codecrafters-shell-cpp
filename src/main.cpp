b774581f70341356fe1bf7112201abe293079d9c
armoredvortex <66690593+ArmoredVortex@users.noreply.github.com>
2026-03-30 01:22:26 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,14 @@ int main() {
   while(true){
     std::cout << "$ ";
     std::string command;
-    std::cin >> command;
+    std::getline(std::cin, command);
     
     if(command == "exit"){
       break;
+    } else if(command.substr(0, 4) == "echo" || command.substr(0, 5) == "echo "){
+      std::cout << command.substr(5, command.size()-5) << '\n';
+    }else {
+      std::cout << command << ": command not found\n";
     }
-    std::cout << command << ": command not found\n";
   }
 }
