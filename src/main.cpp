52df7559894d8d9f8b1a515a3d9a14668eea5f63
armoredvortex <66690593+ArmoredVortex@users.noreply.github.com>
2026-03-30 01:41:50 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 #include <string>
+#include <functional>
+#include <map>
+
+int c_exit(std::string args){
+  return -1;
+}
+
+int c_echo(std::string args){
+  std::cout << args << '\n';
+  return 0;
+}
+
+std::map<std::string, std::function<int(std::string)>> commands = {
+  {"echo", c_echo},
+  {"exit", c_exit}
+};
+
+bool isValidCommand(std::string command){
+  return (commands.find(command) != commands.end());
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -12,11 +32,17 @@ int main() {
     std::string command;
     std::getline(std::cin, command);
     
-    if(command == "exit"){
-      break;
-    } else if(command.substr(0, 4) == "echo" || command.substr(0, 5) == "echo "){
-      std::cout << command.substr(5, command.size()-5) << '\n';
-    }else {
+    std::string cmd = command.substr(0, command.find(' '));
+    std::string args;
+    if (command.find(' ') != std::string::npos){
+      args = command.substr(command.find(' ')+1, command.size() - command.find(' ')-1);
+    }
+
+    if(isValidCommand(cmd)){
+      if(commands[cmd](args) == -1){
+        return 0;
+      };
+    } else {
       std::cout << command << ": command not found\n";
     }
   }
